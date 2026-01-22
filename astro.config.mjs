80f1b14214621a3348bf02c211eded4d204d1593
Matteo Pieroni <matteopieroni6@gmail.com>
2026-01-22 15:22:30 +0000

    chore: add env loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
+
+loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({});
