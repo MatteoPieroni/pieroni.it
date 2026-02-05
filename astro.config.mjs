5d46254638354b2b9921289eefd20717f55f8f49
Matteo Pieroni <matteopieroni6@gmail.com>
2026-02-05 18:49:50 +0000

    feat: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon(), pagefind()],
+});
