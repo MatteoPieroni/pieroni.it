e9bef1e179519438f84d618e9599b1da264aeac1
Matteo Pieroni <matteopieroni6@gmail.com>
2026-02-06 12:58:02 +0000

    feat: switch to @unpic component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import pagefind from "astro-pagefind";
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), pagefind()],
+  image: {
+    service: imageService(),
+  },
 });
