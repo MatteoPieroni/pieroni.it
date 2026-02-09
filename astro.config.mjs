fe4de3f36833ae987a864fa3c3a905626803f92b
Matteo Pieroni <matteopieroni6@gmail.com>
2026-02-09 14:01:52 +0000

    chore: compile for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [icon(), pagefind()],
   adapter: cloudflare({
-    imageService: 'cloudflare'
+    imageService: 'compile'
   }),
 });
