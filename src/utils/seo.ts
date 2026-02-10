a496538f9d3867648cecab297338d7daac9bbe8e
Matteo Pieroni <matteopieroni6@gmail.com>
2026-02-10 19:19:29 +0000

    feat(Blog): add pages

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -2,6 +2,7 @@ export const getMetaDescriptionFromHtmlDescription = (description?: string) => {
   return (
     description
       ?.replace(/<[^>]+>/g, '')
-      .replace(/&#(\d+);/g, (m, d) => String.fromCharCode(d)) || ''
+      .replace(/&#(\d+);/g, (m, d) => String.fromCharCode(d))
+      ?.replace(/\[\&hellip\;\]/g, '') || ''
   );
 };
