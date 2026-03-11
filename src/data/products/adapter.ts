8d4ef5efe714f2a378fc588c579f649ae2fea282
Matteo Pieroni <matteopieroni6@gmail.com>
2026-03-11 17:46:20 +0000

    feat(db): add schema to calls for cats and prods

--- src/data/products/adapter.ts
+++ src/data/products/adapter.ts
@@ -1,24 +1,102 @@
-import type { Category } from '../types';
+import * as z from "zod";
 
-export const getProductsInCategory = async (
-  category: Pick<Category, 'id'>,
-  pageLimit = 100,
-  page = 1,
-) => {
-  const USER_KEY = import.meta.env.USER_KEY;
-  const USER_SECRET = import.meta.env.USER_SECRET;
-  const WOOCOMMERCE_URL = 'https://api.pieroni.it/wp-json/wc/v3';
+const MediaSchema = z.object({
+  id: z.number(),
+  alt: z.string(),
+  url: z.string(),
+});
 
-  const auth = Buffer.from(`${USER_KEY}:${USER_SECRET}`).toString('base64');
-  const products: any[] = [];
+const CategorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  slug: z.string(),
+  featured_image: z.nullable(MediaSchema),
+  count: z.number(),
+  fullSlug: z.string(),
+  level: z.number(),
+  breadcrumbs: z.array(
+    z.nullable(
+      z.object({
+        url: z.string(),
+        label: z.string(),
+      }),
+    ),
+  ),
+  parent: z.nullable(z.union([z.number(), z.object({ id: z.number() })])),
+});
+
+const ProductSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  slug: z.string(),
+  description: z.string(),
+  fullDescription: z.object({}),
+  images: z.nullable(z.array(MediaSchema)),
+  mainCategory: CategorySchema,
+  categories: z.array(CategorySchema),
+  formats: z.nullable(z.string()),
+  brand: z.nullable(z.string()),
+});
+
+export const getCategories = async (pageLimit = 30, page = 1) => {
+  const API_KEY = import.meta.env.API_KEY;
+  const BE_URL = "https://be.pieroni.it/api";
+
+  const categories: z.infer<typeof CategorySchema>[] = [];
 
   while (true) {
     const response = await fetch(
-      `${WOOCOMMERCE_URL}/products?category=${category.id}&per_page=${pageLimit}&page=${page}&status=publish`,
+      `${BE_URL}/shop_categories?limit=${pageLimit}&page=${page}&depth=1`,
       {
         headers: {
-          Authorization: `Basic ${auth}`,
-          'Content-Type': 'application/json',
+          Authorization: `users API-Key ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    if (!response.ok) {
+      break;
+    }
+
+    const fetchedCategories = await response.json();
+    if (fetchedCategories.docs.length === 0) {
+      break;
+    }
+
+    for (const fetchedCategory of fetchedCategories.docs) {
+      categories.push(CategorySchema.parse(fetchedCategory));
+    }
+
+    categories.push(...fetchedCategories.docs);
+
+    if (fetchedCategories.length < pageLimit) {
+      break;
+    }
+
+    page++;
+  }
+
+  return categories;
+};
+
+export const getProductsInCategory = async (
+  category: number,
+  pageLimit = 100,
+  page = 1,
+) => {
+  const API_KEY = import.meta.env.API_KEY;
+  const BE_URL = "https://be.pieroni.it/api";
+
+  const products: z.infer<typeof ProductSchema>[] = [];
+
+  while (true) {
+    const response = await fetch(
+      `${BE_URL}/shop_products?limit=${pageLimit}&page=${page}&depth=3&where[categories][contains]=${category}`,
+      {
+        headers: {
+          Authorization: `users API-Key ${API_KEY}`,
+          "Content-Type": "application/json",
         },
       },
     );
@@ -28,11 +106,13 @@ export const getProductsInCategory = async (
     }
 
     const pageProducts = await response.json();
-    if (pageProducts.length === 0) {
+    if (pageProducts.docs.length === 0) {
       break;
     }
 
-    products.push(...pageProducts);
+    for (const pageProduct of pageProducts.docs) {
+      products.push(ProductSchema.parse(pageProduct));
+    }
 
     if (pageProducts.length < pageLimit) {
       break;
