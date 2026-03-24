563832ad6a0e8b794503d3125e91b444c7a566be
Matteo Pieroni <matteopieroni6@gmail.com>
2026-03-24 10:29:37 +0000

    feat(blog): redirects

--- src/config/redirects.ts
+++ src/config/redirects.ts
@@ -1,65 +1,79 @@
 export const redirects = {
-  '/materiali-edili-attrezzature/': 'https://www.pieroni.it/materiali-edili/',
-  '/sala-mostra-bagno/cabine-piatti-doccia':
-    'https://www.pieroni.it/negozio/bagno/piatti-doccia/',
-  '/sala-mostra-bagno/vasche-da-bagno':
-    'https://www.pieroni.it/negozio/bagno/vasche-da-bagno/',
-  '/sala-mostra-bagno/mobili-arredo-bagno-sanitari':
-    'https://www.pieroni.it/negozio/bagno/mobili-bagno/',
-  '/sala-mostra-bagno/accessori-bagno-rubinetteria':
-    'https://www.pieroni.it/negozio/bagno/accessori-bagno/',
-  '/sala-mostra-bagno/': 'https://www.pieroni.it/arredo-bagno/',
-  '/mattonelle/pavimenti-cotto-gres-porcellanato':
-    'https://www.pieroni.it/negozio/piastrelle/pavimenti/',
-  '/mattonelle/rivestimenti-piastrelle-bagno':
-    'https://www.pieroni.it/negozio/piastrelle/rivestimenti-bagno/',
-  '/mattonelle/piastrelle-pavimenti-da-esterno':
-    'https://www.pieroni.it/negozio/piastrelle/piastrelle-per-esterno/',
-  '/mattonelle/': 'https://www.pieroni.it/piastrelle-rivestimenti-bagno/',
-  '/riscaldamento/stufe-a-pellet-e-legna':
-    'https://www.pieroni.it/negozio/riscaldamento/stufe-a-pellet-e-legna/',
-  '/riscaldamento/inserti-camini-pellet-legna':
-    'https://www.pieroni.it/negozio/riscaldamento/camini-inserti/',
-  '/riscaldamento/caldaie-pellet-legna':
-    'https://www.pieroni.it/negozio/riscaldamento/caldaie-a-pellet/',
-  '/riscaldamento/offerta-pellet-prestagionale-prezzi/':
-    'https://www.pieroni.it/negozio/riscaldamento/pellet-prezzi-offerta/',
-  '/riscaldamento/': 'https://www.pieroni.it/stufe-caldaie-pellet/',
-  '/ferramenta-utensileria/': 'https://www.pieroni.it/ferramenta/',
-  '/outlet/': 'https://outlet.pieroni.it',
-  '/dove-siamo-magazzino-edile/diecimo-lucca/':
-    'https://www.pieroni.it/magazzino-edile/diecimo-borgo-a-mozzano/',
-  '/dove-siamo-magazzino-edile/fornaci-di-barga-lucca/':
-    'https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/',
-  '/dove-siamo-magazzino-edile/lucca/':
-    'https://www.pieroni.it/magazzino-edile/lucca-capannori/',
-  '/dove-siamo-magazzino-edile/': 'https://www.pieroni.it/magazzino-edile/',
-  '/category/speciali/eventi/': 'https://www.pieroni.it/category/eventi/',
-  '/category/speciali/forni-e-barbecue/':
-    'https://www.pieroni.it/category/gli-speciali/forni-e-barbecue/',
-  '/category/speciali/pavimenti/':
-    'https://www.pieroni.it/category/gli-speciali/pavimenti/',
-  '/category/speciali/riscaldamento/':
-    'https://www.pieroni.it/category/gli-speciali/riscaldamento/',
-  '/category/speciali/': 'https://www.pieroni.it/category/gli-speciali/',
-  '/conto-termico-2018-incentivo-per-il-risparmio-energetico/':
-    'https://www.pieroni.it/category/gli-speciali/conto-termico-2018-incentivo-per-il-risparmio-energetico/',
-  '/detrazioni-ristrutturazioni-bonus-energetico-antisismico-2018-la-nostra-guida/':
-    'https://www.pieroni.it/category/gli-speciali/detrazioni-ristrutturazioni-bonus-energetico-e-antisismico-2018-la-nostra-guida/',
-  '/forni-da-esterno/':
-    'https://www.pieroni.it/category/gli-speciali/forni-e-barbecue/',
-  '/stufa-pellet-tutto-compreso-989-euro/':
-    'https://www.pieroni.it/negozio/riscaldamento/stufe-a-pellet-e-legna/',
-  '/dove-siamo/diecimo/':
-    'https://www.pieroni.it/magazzino-edile/diecimo-borgo-a-mozzano/',
-  '/dove-siamo/fornaci/':
-    'https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/',
-  '/dove-siamo/': 'https://www.pieroni.it/magazzino-edile/',
-  '/offerta-pellet-prestagionale-prezzi/':
-    'https://www.pieroni.it/negozio/riscaldamento/pellet-prezzi-offerta/',
-  '/magazzino-edile/fornaci-di-barga/':
-    'https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/',
-  '/tv/': 'https://www.pieroni.it/',
-  '/eventi/': 'https://www.pieroni.it/professionisti-edilizia/',
-  '/primo-maggio/': 'https://www.pieroni.it',
+  "/materiali-edili-attrezzature/": "https://www.pieroni.it/materiali-edili/",
+  "/sala-mostra-bagno/cabine-piatti-doccia":
+    "https://www.pieroni.it/negozio/bagno/piatti-doccia/",
+  "/sala-mostra-bagno/vasche-da-bagno":
+    "https://www.pieroni.it/negozio/bagno/vasche-da-bagno/",
+  "/sala-mostra-bagno/mobili-arredo-bagno-sanitari":
+    "https://www.pieroni.it/negozio/bagno/mobili-bagno/",
+  "/sala-mostra-bagno/accessori-bagno-rubinetteria":
+    "https://www.pieroni.it/negozio/bagno/accessori-bagno/",
+  "/sala-mostra-bagno/": "https://www.pieroni.it/arredo-bagno/",
+  "/mattonelle/pavimenti-cotto-gres-porcellanato":
+    "https://www.pieroni.it/negozio/piastrelle/pavimenti/",
+  "/mattonelle/rivestimenti-piastrelle-bagno":
+    "https://www.pieroni.it/negozio/piastrelle/rivestimenti-bagno/",
+  "/mattonelle/piastrelle-pavimenti-da-esterno":
+    "https://www.pieroni.it/negozio/piastrelle/piastrelle-per-esterno/",
+  "/mattonelle/": "https://www.pieroni.it/piastrelle-rivestimenti-bagno/",
+  "/riscaldamento/stufe-a-pellet-e-legna":
+    "https://www.pieroni.it/negozio/riscaldamento/stufe-a-pellet-e-legna/",
+  "/riscaldamento/inserti-camini-pellet-legna":
+    "https://www.pieroni.it/negozio/riscaldamento/camini-inserti/",
+  "/riscaldamento/caldaie-pellet-legna":
+    "https://www.pieroni.it/negozio/riscaldamento/caldaie-a-pellet/",
+  "/riscaldamento/offerta-pellet-prestagionale-prezzi/":
+    "https://www.pieroni.it/negozio/riscaldamento/pellet-prezzi-offerta/",
+  "/riscaldamento/": "https://www.pieroni.it/stufe-caldaie-pellet/",
+  "/ferramenta-utensileria/": "https://www.pieroni.it/ferramenta/",
+  "/outlet/": "https://outlet.pieroni.it",
+  "/dove-siamo-magazzino-edile/diecimo-lucca/":
+    "https://www.pieroni.it/magazzino-edile/diecimo-borgo-a-mozzano/",
+  "/dove-siamo-magazzino-edile/fornaci-di-barga-lucca/":
+    "https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/",
+  "/dove-siamo-magazzino-edile/lucca/":
+    "https://www.pieroni.it/magazzino-edile/lucca-capannori/",
+  "/dove-siamo-magazzino-edile/": "https://www.pieroni.it/magazzino-edile/",
+  "/category/speciali/eventi/": "https://www.pieroni.it/category/eventi/",
+  "/category/speciali/forni-e-barbecue/":
+    "https://www.pieroni.it/category/gli-speciali/forni-e-barbecue/",
+  "/category/speciali/pavimenti/":
+    "https://www.pieroni.it/category/gli-speciali/pavimenti/",
+  "/category/speciali/riscaldamento/":
+    "https://www.pieroni.it/category/gli-speciali/riscaldamento/",
+  "/category/speciali/": "https://www.pieroni.it/category/gli-speciali/",
+  "/conto-termico-2018-incentivo-per-il-risparmio-energetico/":
+    "https://www.pieroni.it/category/gli-speciali/conto-termico-2018-incentivo-per-il-risparmio-energetico/",
+  "/detrazioni-ristrutturazioni-bonus-energetico-antisismico-2018-la-nostra-guida/":
+    "https://www.pieroni.it/category/gli-speciali/detrazioni-ristrutturazioni-bonus-energetico-e-antisismico-2018-la-nostra-guida/",
+  "/forni-da-esterno/":
+    "https://www.pieroni.it/category/gli-speciali/forni-e-barbecue/",
+  "/stufa-pellet-tutto-compreso-989-euro/":
+    "https://www.pieroni.it/negozio/riscaldamento/stufe-a-pellet-e-legna/",
+  "/dove-siamo/diecimo/":
+    "https://www.pieroni.it/magazzino-edile/diecimo-borgo-a-mozzano/",
+  "/dove-siamo/fornaci/":
+    "https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/",
+  "/dove-siamo/": "https://www.pieroni.it/magazzino-edile/",
+  "/offerta-pellet-prestagionale-prezzi/":
+    "https://www.pieroni.it/negozio/riscaldamento/pellet-prezzi-offerta/",
+  "/magazzino-edile/fornaci-di-barga/":
+    "https://www.pieroni.it/category/blog/pieroni-srl/nuovi-progetti-per-il-futuro/",
+  "/tv/": "https://www.pieroni.it/",
+  "/eventi/": "https://www.pieroni.it/professionisti-edilizia/",
+  "/primo-maggio/": "https://www.pieroni.it",
+
+  // old articles
+  "/category/blog/tanti-auguri/": "https://www.pieroni.it/category/blog/",
+  "/category/tanti-auguri/": "https://www.pieroni.it/category/blog/",
+  "/category/blog/siamo-alla-ricerca-di-nuovi-collaboratori/":
+    "https://www.pieroni.it/category/blog/",
+  "/category/siamo-alla-ricerca-di-nuovi-collaboratori/":
+    "https://www.pieroni.it/category/blog/",
+  "/category/blog/iris/": "https://www.pieroni.it/category/blog/",
+  "/category/iris/": "https://www.pieroni.it/category/blog/",
+  "/category/blog/wemo-controlla-la-tua-casa-con-lo-smartphone/":
+    "https://www.pieroni.it/category/blog/",
+  "/category/wemo-controlla-la-tua-casa-con-lo-smartphone/":
+    "https://www.pieroni.it/category/blog/",
 };
